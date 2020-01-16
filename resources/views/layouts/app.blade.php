70ef388a13c47b7344706506f83580e539944e43
Mike van Egmond <m.vanegmond@freave.com>
2020-01-16 22:53:41 +0100

    Added basic file layout + new font.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Stage Tracker') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/306091d7c8.js" crossorigin="anonymous"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+
+        @include('includes.header')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        @include('includes.footer')
+    </div>
+</body>
+</html>

745bf0e5ac39c44267ead4c7dc3ee221fb2e6a67
Niels Bosman <nielsbosman33@icloud.com>
2020-01-20 14:34:06 +0100

    Footer partial + responsive

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,25 @@
-<footer>
-    <h1>Footer</h1>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 footer__col">
+                <h3 class="footer__heading">Menu</h3>
+                <ul class="footer__list">
+                    <li class="footer__list-item footer__text">Home</li>
+                    <li class="footer__list-item footer__text">Uren indienen</li>
+                    <li class="footer__list-item footer__text">Overzicht</li>
+                </ul>
+            </div>
+            <div class="col-md-3 footer__col">
+                <h3 class="footer__heading">Account</h3>
+                <ul class="footer__list">
+                    <li class="footer__list-item footer__text">Profiel</li>
+                    <li class="footer__list-item footer__text">Inloggen</li>
+                    <li class="footer__list-item footer__text">Uitloggen</li>
+                    <li class="footer__list-item footer__text">Wachtwoord vergeten</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <p class="footer__text footer__copyright">Copyright {{date('Y')}} &copy; Stage Tracker</p>
+    </div>
 </footer>

1b5910ee1fd3a754b9efc964b6def9ba43cc6795
Niels Bosman <nielsbosman33@icloud.com>
2020-01-20 16:56:20 +0100

    Changes to padding for calendar row

--- public/js/app.js
+++ public/js/app.js
@@ -2007,6 +2007,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "WeekCalendarComponent",
   mounted: function mounted() {
